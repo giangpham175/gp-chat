4cd47fa850cedeedc53f6128ac4e0ea7518e1b9f
Giang <14110040@student.hcmute.edu.vn>
2020-11-15 16:56:54 +0700

    design chat form

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="chat container">
-        <h2>hi {{this.name}} - chat everything</h2>
+        <h2 class="center teal-text">GP Chat</h2>
+        
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">message</span>
+                        <span class="grey-text time">time</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="card-action">
+                <input type="text">
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -18,5 +35,17 @@ export default {
 </script>
 
 <style>
+.chat h2 {
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
 
+.chat span {
+    font-size: 1.4em;
+}
+
+.chat .time {
+    display: block;
+    font-size: 1.2em;
+}
 </style>
